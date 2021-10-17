7959d858ec9ae6ecfffd79fbe9461b46c4a16055
Luis Carballo <g18ada13@hackademy.mx>
2021-10-17 17:42:33 -0500

    En este commit acomode la pantalla para que se vea de una manera ordenada, agrege el ojo de ocultar y agregar contraseña, a assets agregue las imagenes SONIAPP y los ojos de ocultar/agregar

--- Views/LoginScreen.js
+++ Views/LoginScreen.js
@@ -1,10 +1,15 @@
 import React, {Component} from 'react';
-import {Text, View, Button,StyleSheet,TextInput} from 'react-native';
+import {Text, View, Button,StyleSheet,TextInput,TouchableOpacity, Image} from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import imageSoni from '../assets/SONIAPP.png'
+import imageVer from '../assets/ojoVer.png'
+import imageOcultar from '../assets/ojoOculto.png'
 
 const LoginScreen=({navigation})=>{
 
     const [user, setUser] = React.useState('');
+    const [shown, setShown] = React.useState(false);
+    const [ojo, setOjo] = React.useState(false);
 
     _Login=async () =>{
         try{
@@ -14,33 +19,91 @@ const LoginScreen=({navigation})=>{
         
     }
 
+    const mostrarContraseña = () =>setShown(!shown);
+    const mostrarOjo = () =>setOjo(!ojo);
+    const funcionesContraseña=() =>{
+        mostrarContraseña();
+        mostrarOjo();
+    };
+
     return(
+        
         <View style={styles.view}>
+            <View style={styles.soniapp}>
+                <Image source={imageSoni} style={styles.imageSonia}/>
+            </View>
             <View style={styles.form}>
-                <TextInput placeholder={'Ingresa tu correo'} onChangeText={setUser}></TextInput>
-                <TextInput placeholder={'Ingresa tu Contraseña'}></TextInput>
+                <TextInput placeholder={'Ingresa tu correo'} onChangeText={setUser}/>
             </View>
-            <Button title={"Inicia sesion"} onPress={()=>{_Login()}}></Button>
-            <Button title={"Recuperar contraseña"} onPress={()=>{
-                navigation.navigate('RegisterScreen')
-            }}></Button>
+            <View style={styles.row}>
+                <TextInput placeholder={'Ingresa tu Contraseña'} style={{marginRight: 10}} secureTextEntry={shown ? false : true} />
+                <TouchableOpacity onPress={funcionesContraseña}>
+                    <Image source={ojo ? imageVer : imageOcultar} style={styles.imageOjito}/>
+                </TouchableOpacity>
             </View>
+            <TouchableOpacity style={styles.bottonRecuperar} onPress={()=>{navigation.navigate('RegisterScreen')}}>
+                <Text> Olvide mi Contraseña </Text>
+            </TouchableOpacity>
+            <TouchableOpacity  style={styles.bottonLogin} onPress={()=>{_Login()}}>
+                <Text style={styles.textoLogin}>       Login </Text>
+            </TouchableOpacity>
+        </View>
         );
 }
 export default LoginScreen;
 const styles = StyleSheet.create({
-   view:{
-       flex: 1,
-       alignItems: 'center',
-       justifyContent: 'center',
-   },
+    imageSonia:{
+        height: 200,
+        width: 250,
+    },
+    imageOjito:{
+        height: 30,
+        width: 30,
+    },
+    view:{
+        flex: 1,
+        justifyContent: 'center',
+    },
+    soniapp:{
+        flex:4,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
    form:{
-       marginTop: 4,
+       flex: 2,
+       marginBottom: 20,
+       alignItems: 'center',
+       justifyContent: 'flex-end',
    },
-   botton:{
-       marginTop: 10,
-       backgroundColor: '#00b7b8',
+   row: {
+    flex: 1,
+    flexDirection: "row",
+    flexWrap: "wrap",
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginRight: 20,
+  },
+  textoLogin:{
+        color: '#ffffff',
+        fontFamily: ('Poppins', 'sans-serif'),
+        borderRadius: 4,
+        alignSelf: "center",
+        minWidth: "20%",
+        paddingVertical: 5,
+        backgroundColor: '#00b7b8', 
+        height: 40,
+        width: 50,
+  },
+  bottonLogin:{
+        flex: 4,
+        marginTop: 40,
+        alignItems: 'center',
+   },
+   bottonRecuperar:{
+       flex: 0,
+       marginTop: 20,
        color: '#ffffff',
        fontFamily: ('Poppins', 'sans-serif'),
-   }
+       alignItems: 'flex-end',
+   },
 });
