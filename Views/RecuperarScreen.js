663722702832ce045e1fe8f94a59ef13ea61935c
Luis Carballo <g18ada13@hackademy.mx>
2021-10-26 18:55:42 -0500

    agreo la base del screen recuperar contraseña

--- Views/RecuperarScreen.js
+++ Views/RecuperarScreen.js
@@ -1,13 +1,21 @@
 import React, {Component} from 'react';
-import {Text, View, StyleSheet,TouchableOpacity,Image} from 'react-native';
+import {Text, View, StyleSheet,TouchableOpacity,Image,TextInput} from 'react-native';
 import { Card, Paragraph, Avatar,Button} from 'react-native-paper';
 import imageSoni from '../assets/SONIAPP.png'
 
 const RegisterScreen =({navigation})=>{
 
+    const [correo,setCorreo]=React.useState('');
+
     _Regresar=()=>{
         navigation.navigate('LoginScreen');
     }
+
+    _Regristro=()=>{
+        navigation.navigate('LoginScreen');
+        
+    }
+
 
         return(
             <View style={styles.view}>
@@ -17,6 +25,12 @@ const RegisterScreen =({navigation})=>{
                 <View style={styles.soniapp}>
                     <Image source={imageSoni} style={styles.imageSonia}/>
                 </View>
+                <View style={styles.form1}>
+                    <TextInput placeholder={'Ingresa tu correo'} style={styles.cuadroTexto} placeholderTextColor={'black'} autoCapitalize='none' onChangeText={setCorreo}/>
+                </View>
+                <TouchableOpacity style={styles.form2} onPress={()=>{_Regristro()}}>
+                    <Text style={styles.textoBotonRecuperar}> Recuperar Contraseña </Text>
+                </TouchableOpacity>
             </View>
         );
 }
@@ -28,14 +42,55 @@ const styles = StyleSheet.create({
    flecha:{
        flex:1,
        alignItems: 'flex-start',
+       justifyContent: 'center',
+       transform: [{ scale: 1.5 },{ translateX: 55 }]
    },
     soniapp:{
-        flex:4,
+        flex:1,
         alignItems: 'center',
         justifyContent: 'center',
+        marginTop: 50,
     },
     imageSonia:{
         height: 200,
         width: 250,
     },
+    form1:{
+       flex: 1,
+       marginBottom: 20,
+       alignItems: 'flex-start',
+       justifyContent: 'flex-end',
+       marginBottom: 50,
+   },
+   form2:{
+       flex: 2,
+       marginBottom: 20,
+       alignItems: 'flex-start',
+       justifyContent: 'flex-start',
+   },
+    cuadroTexto:{
+        flex:0,
+        color: '#000000',
+        fontFamily: ('Poppins', 'sans-serif'),
+        borderRadius: 10,
+        alignSelf: "center",
+        minWidth: "50%",
+        paddingVertical: 5,
+        paddingHorizontal: 6,
+        backgroundColor: '#ffffff', 
+        marginRight: 10,
+        borderWidth: 1,
+    },
+    textoBotonRecuperar:{
+        color: '#ffffff',
+        fontFamily: ('Poppins', 'sans-serif'),
+        borderRadius: 10,
+        alignSelf: "center",
+        alignItems: "flex-start",
+        minWidth: "55%",
+        paddingVertical: 7,
+        backgroundColor: '#00b7b8', 
+        height: 35,
+
+  },
 });
